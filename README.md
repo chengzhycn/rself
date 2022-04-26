7dfaee0f6118b485049b0319d681df855d97e7a6
chengzhycn <chengzhycn@gmail.com>
2022-04-26 08:16:10 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rself
+
+## Dependencies
+
+* [clap](https://github.com/clap-rs/clap) - A full featured, fast Command Line Argument Parser for Rust.
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+// const EI_NIDENT: usize = 16;
+//
+// struct Elf64_Ehdr {
+//     e_ident: [u8; EI_NIDENT],
+//     e_type: u16,
+//     e_machine: u16,
+//     e_version: u32,
+//     e_entry: u64,
+//     e_phoff: u64,
+//     e_shoff: u64,
+//     e_flags: u32,
+//     e_ehsize: u16,
+//     e_phentsize: u16,
+//     e_phnum: u16,
+//     e_shensize: u16,
+//     e_shnum: u16,
+//     e_shstrndx: u16,
+// }
+//
+// impl Elf64_Ehdr {
+//     fn from_file(path: &str) -> Elf64_Ehdr {}
+// }
